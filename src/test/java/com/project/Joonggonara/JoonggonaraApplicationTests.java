13702d3e9c3c1d2532fc6007de5ab544bb8228a8
cotmdgns <qmflskzm@gmail.com>
2026-09-22 09:15:41 +0900

    boot setting start

---
+++ src/test/java/com/project/Joonggonara/JoonggonaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Joonggonara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoonggonaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
